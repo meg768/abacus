033025a0c30de063b4eeaf64d84487b69360ec43
meg768 <magnus@egelberg.se>
2017-05-02 17:56:02 +0200

    -

---
+++ src/scripts/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {IndexRoute, hashHistory, Router, Route} from 'react-router';
+
+
+require('../less/styles.less');
+
+export class App extends React.Component {
+
+  render() {
+    return (
+		<div className='app'>
+			{this.props.children}
+    	</div>
+    );
+  }
+};
+
+
+ReactDOM.render((
+	<Router history={hashHistory}>
+		<Route path="/" component={App}>
+			<IndexRoute component={require('../pages/home/home.js')} />
+			<Route path="home"          component={require('../pages/home/home.js')} />
+			<Route path="about"         component={require('../pages/about/about.js')} />
+
+		</Route>
+	</Router>
+), document.getElementById('app'))
